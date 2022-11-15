1cd74210411d4ad56007f3eaf4d77b076d1e4014
hengxiao <hengxiao@iflytek.com>
2022-11-15 22:29:03 +0800

    🦄 refactor(packages): 重新编译

--- packages/utils/dist/index.cjs.js
+++ packages/utils/dist/index.cjs.js
@@ -4,7 +4,7 @@ var log = require('npmlog');
 
 log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info';
 
-log.heading = 'js-cli';
+log.heading = 'monorepo-cli';
 log.addLevel('success', 2000, {fg: 'green', bold: true});
 
 exports.log = log;
--- packages/utils/dist/index.umd.js
+++ packages/utils/dist/index.umd.js
@@ -6,7 +6,7 @@
 
 	log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info';
 
-	log.heading = 'js-cli';
+	log.heading = 'monorepo-cli';
 	log.addLevel('success', 2000, {fg: 'green', bold: true});
 
 	exports.log = log;
